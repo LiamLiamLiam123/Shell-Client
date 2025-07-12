5e21b9250713d3bc4498c457d7207b20b6c202fa
LiamLiamLiam123 <163282026+LiamLiamLiam123@users.noreply.github.com>
2025-07-12 16:29:28 +1000

    Create PongCommand.java

---
+++ src/main/java/me/alpha432/oyvey/features/commands/impl/PongCommand.java
@@ -0,0 +1,19 @@
+package me.alpha432.oyvey.features.commands.impl;
+
+import me.alpha432.oyvey.OyVey;
+import me.alpha432.oyvey.features.commands.Command;
+import net.minecraft.util.Formatting;
+
+public class PongCommand
+        extends Command {
+    public PongCommand() {
+        super("ping");
+    }
+    @Override
+    public void execute(String[] commands) {
+        PongCommand.sendMessage("Pong!");
+            }
+            HelpCommand.sendMessage(builder.toString());
+        }
+    }
+}
