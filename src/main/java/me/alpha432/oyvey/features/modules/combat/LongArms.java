b32e8423d446ee58e8f63d53b535e66741cf9104
LiamLiamLiam123 <163282026+LiamLiamLiam123@users.noreply.github.com>
2025-07-30 12:06:11 +1000

    Update LongArms.java

--- src/main/java/me/alpha432/oyvey/features/modules/combat/LongArms.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/LongArms.java
@@ -1,7 +1,7 @@
 package me.alpha432.oyvey.features.modules.combat;
 
 import me.alpha432.oyvey.features.modules.Module;
-import me.alpha432.oyvey.features.setting.Setting;
+import me.alpha432.oyvey.features.settings.Setting;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.client.MinecraftClient;
