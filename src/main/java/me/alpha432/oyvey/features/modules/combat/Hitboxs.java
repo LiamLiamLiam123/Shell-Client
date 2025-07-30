bd2ba3c9d28044994316852ef95f837d8df165b5
LiamLiamLiam123 <163282026+LiamLiamLiam123@users.noreply.github.com>
2025-07-30 12:55:51 +1000

    Update Hitboxs.java

--- src/main/java/me/alpha432/oyvey/features/modules/combat/Hitboxs.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/Hitboxs.java
@@ -3,15 +3,15 @@ package me.alpha432.oyvey.features.modules.combat;
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.Matrix4f;
+import org.joml.Matrix4f;
 
 public class Hitboxs extends Module {
     public Setting<Double> extendSize = this.register(new Setting<>("ExtendSize", 0.5, 0.1, 2.0));
@@ -47,7 +47,7 @@ public class Hitboxs extends Module {
         float y2 = (float) box.maxY;
         float z2 = (float) box.maxZ;
 
-        // Vertical lines
+        // Vertical edges
         drawLine(consumer, matrix, x1, y1, z1, x1, y2, z1, r, g, b, alpha);
         drawLine(consumer, matrix, x2, y1, z1, x2, y2, z1, r, g, b, alpha);
         drawLine(consumer, matrix, x1, y1, z2, x1, y2, z2, r, g, b, alpha);
@@ -66,7 +66,8 @@ public class Hitboxs extends Module {
         drawLine(consumer, matrix, x1, y2, z2, x1, y2, z1, r, g, b, alpha);
     }
 
-    private void drawLine(VertexConsumer consumer, Matrix4f matrix, float x1, float y1, float z1, float x2, float y2, float z2, float r, float g, float b, float alpha) {
+    private void drawLine(VertexConsumer consumer, Matrix4f matrix, float x1, float y1, float z1,
+                          float x2, float y2, float z2, float r, float g, float b, float alpha) {
         consumer.vertex(matrix, x1, y1, z1).color(r, g, b, alpha).next();
         consumer.vertex(matrix, x2, y2, z2).color(r, g, b, alpha).next();
     }
