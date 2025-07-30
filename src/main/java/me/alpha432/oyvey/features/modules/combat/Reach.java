b1159f1d008c2e5ad0ebff904e873310c8bfa098
LiamLiamLiam123 <163282026+LiamLiamLiam123@users.noreply.github.com>
2025-07-30 11:51:30 +1000

    Create Reach.java

---
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/Reach.java
@@ -0,0 +1,27 @@
+package me.alpha432.oyvey.features.modules.combat;
+
+import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.setting.Setting;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.client.MinecraftClient;
+
+public class ReachExtender extends Module {
+    public Setting<Double> reach = this.register(new Setting<>("LongArms", 4.5, 3.0, 10.0));
+
+    public ReachExtender() {
+        super("LongArms", "Increases the length of your Reach.", Category.COMBAT, true, false, false);
+    }
+
+    // Call this method when calculating attack reach, example in attack logic
+    public double getReach() {
+        return reach.getValue();
+    }
+
+    // Example usage: replace vanilla reach check in your attack handling with this method
+    public boolean canReachEntity(PlayerEntity player, PlayerEntity target) {
+        double distSq = player.squaredDistanceTo(target);
+        double maxReach = getReach();
+        return distSq <= maxReach * maxReach;
+    }
+}
