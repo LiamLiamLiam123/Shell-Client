bff89e73976dd0eeacd9e8ba64747a0c25d64132
LiamLiamLiam123 <163282026+LiamLiamLiam123@users.noreply.github.com>
2025-07-30 12:26:54 +1000

    Update ModuleManager.java

--- src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
+++ src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
@@ -42,7 +42,7 @@ public class ModuleManager implements Jsonable, Util {
         modules.add(new BlockHighlight());
         modules.add(new NoFall());
         modules.add(new LongArms());
-        modules.add(new Hitboxs());
+        modules.add(new Hitboxs());'
 
     }
 
